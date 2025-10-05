1a04199ea28a86219a7fd8c4539b00e18f34ffdd
vansh-nagar <189650812+vansh-nagar@users.noreply.github.com>
2025-10-05 09:19:31 +0530

    feat: add CORS support for Socket.io connections

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,18 @@
-import express from "express";
 const server = require("http").createServer();
-const io = require("socket.io")(server);
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
 
 io.on("connection", (socket: any) => {
   console.log("a user connected");
+
+  socket.on("message", (data: any) => {
+    console.log("Message from client: ", data);
+    io.emit("message", `Server received: ${data}`);
+  });
 });
 
 server.listen(8000, () => {
